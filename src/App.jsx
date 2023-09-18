b49b2bb1ef6ed373a33d006913a2de8f8a32fb91
ronodir84 <priashdas@84gmail.com>
2023-09-18 13:04:29 +0600

    added the my commit and all section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-4xl bg-red-300 text-center'>Vite + React</h1>
+
+
+
+ 
+     
+      
+    </>
+  )
+}
+
+export default App
